cb8c5d13631c2f27e2f749474ca7378f554f1ffa
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2017-03-13 00:22:44 +0800

    tnf upload index

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-test2
+<h1 style="text-align: center;"><strong>SEO HERO Wix Contest</strong></h1>
+<p><strong><strong>&nbsp;</strong></strong></p>
+<p><span style="font-weight: 400;">Everyone was invited to create a website optimized to the keyword, </span><a href="https://www.seohero.support"><strong><span style="font-weight: bold;">SEO Hero</span></strong></a><span style="font-weight: 400;">. They are to create a website in English on any platform (not just wix.com) and must rank their website on the keyword &ldquo;</span><a href="https://www.seohero.support/"><span style="font-weight: 400;">SEO Hero</span></a><span style="font-weight: 400;">&rdquo;. If their website ranks highest for that search term in four months&rsquo; time, WIX will give the winning website $50,000.</span></p>
+<p><strong><strong>&nbsp;</strong></strong></p>
+<p><span style="font-weight: 400;">Every day, a moderator will search Google for the term &ldquo;SEO Hero.&rdquo; The site to rank highest on average over the period of March 13 to 15, 2017 at 12:00PM ET (noon) and 20:00PM ET will be declared winner within two weeks of the challenge end date.</span></p>
+<p><strong><strong>&nbsp;</strong></strong></p>
+<p><a href="https://support.google.com/webmasters/answer/35769?hl=en"><span style="font-weight: 400;">Google&rsquo;s guidelines for SEO</span></a><span style="font-weight: 400;"> will be followed and all participants are expected to comply with the rules of the competition. Eran Hurvitz, CEO of MRKT360 INC, was tasked to ensure that everyone does. Entries must also satisfy the following requirements:</span></p>
+<p><strong><strong>&nbsp;</strong></strong></p>
+<p><span style="font-weight: 400;">(1) Domain must be purchased on or after November 16, 2016. Note that the submission of entries was completed between November 16 and December 16, 2016. The domain cannot have a domain history prior to its current purchase date.</span></p>
+<p><strong><br /><span style="font-weight: 400;">(2) An account with Google Analytics and Google search console by February 16, 2017 to be connected to participant&rsquo;s SEO Hero website </span><span style="font-weight: 400;">and add judgewixseohero@gmail.com as an admin</span><span style="font-weight: 400;">.</span></strong></p>
