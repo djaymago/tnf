0b49ed8133c56ae9a252bddb8dd6c98b6d94fbea
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2017-03-13 00:32:43 +0800

    update font, and meta

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<html>
+<head>
+    <title>Check out and know what SEO HERO Wix Contest is all about!</title>
+    <style>p {font-size: 20px !important;}</style>
+</head>
+<body>
 <h1 style="text-align: center;"><strong>SEO HERO Wix Contest</strong></h1>
 <p><strong><strong>&nbsp;</strong></strong></p>
 <p><span style="font-weight: 400;">Everyone was invited to create a website and join the SEO Hero WIX contest. They are to create a website in English on any platform (not just wix.com) and must rank their website on the keyword &ldquo;</span><strong><a href="https://www.seohero.support/">SEO Hero</a></strong><span style="font-weight: 400;">&rdquo;. If their website ranks highest for that search term in four months&rsquo; time, WIX will give the winning website $50,000.</span></p>
@@ -7,3 +13,5 @@
 <p><a href="https://support.google.com/webmasters/answer/35769?hl=en"><span style="font-weight: 400;">Google&rsquo;s guidelines for SEO</span></a><span style="font-weight: 400;"> will be followed and all participants are expected to comply with the rules of the competition. Eran Hurvitz, CEO of MRKT360 INC, was tasked to ensure that everyone does. Entries must also satisfy the following requirements:</span></p>
 <p><span style="font-weight: 400;">(1) Domain must be purchased on or after November 16, 2016. Note that the submission of entries was completed between November 16 and December 16, 2016. The domain cannot have a domain history prior to its current purchase date.</span></p>
 <p><strong><span style="font-weight: 400;">(2) An account with Google Analytics and Google Search Console by February 16, 2017 to be connected to participant&rsquo;s SEO Hero website </span><span style="font-weight: 400;">and add judgewixseohero@gmail.com as an admin</span><span style="font-weight: 400;">.</span></strong></p>
+</body>
+</html>
